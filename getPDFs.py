07b0f5a67e0ad2e5b76c96ae8996eacc438dceae
Nishant Vijayakumar <nvijay@apptopia.com>
2018-01-24 12:31:32 -0500

    Made code PEP8 compliant (finally), pass url/extension/output_dir as command line arguments

--- getPDFs.py
+++ getPDFs.py
@@ -1,35 +1,54 @@
 """
 v0.1beta
 Scrapes a given 'url' for files of type 'extension' 
-Opens the files and saves them in the same folder as the script
+If directory is passed as parm, saves the files in the specified dir, else defaults to "downloaded_files"
 """
 
 import os
 import urllib
 import re
 import string
+import sys
 from urlparse import urljoin
 from BeautifulSoup import BeautifulSoup
 
+
 def main():
-  url = "http://www.cs.cornell.edu/home/kleinber/"
-  extension = ".pdf"
-  print "Scraping url %s for files of type %s...\n"%(url, extension)
-  soup = BeautifulSoup(urllib.urlopen(url))
-  tags = soup.findAll('a', href=True)
-  for tag in tags:
-    link = tag['href']
-    if "http:" not in link and extension in link: 
-      fqURL = urljoin(url, link) 
-      fileName = ("%s%s"%(re.sub('[%s]' % re.escape(string.punctuation), \
-      '', tag.renderContents()),extension)).replace(" ","_").\
-      lstrip("\n").lower().replace("\n","_")
-      fileContents = (urllib.urlopen(fqURL))
-      fp = open(fileName, "wb")
-      fp.write(fileContents.read())
-      fp.close()
-      os.chmod(fileName, 0644)
-      print "Saved %s..."%fileName
+    try:
+        url = sys.argv[1]
+        ext = sys.argv[2]
+    except IndexError:
+        print 'Usage: python getPDFs.py <url> <extension> <output_dir (optional)>'
+        print 'Example: python getPDFs.py google.com .html'
+        sys.exit(-2)
+    try:
+        output_dir = sys.argv[3]
+    except IndexError:
+        output_dir = "downloaded_files"
+    extension = '.' + ext if not ext[0] == '.' else ext
+    download_files(url, extension, output_dir)
+
+
+def download_files(url, extension, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        os.chmod(output_dir, 0777)
+    print "Scraping url %s for files of type %s...\n" % (url, extension)
+    soup = BeautifulSoup(urllib.urlopen(url))
+    tags = soup.findAll('a', href=True)
+    for tag in tags:
+        link = tag['href']
+        if "http:" not in link and extension in link: 
+            fq_url = urljoin(url, link)
+            file_name = ("%s%s" % (re.sub('[%s]' % re.escape(string.punctuation), '', tag.renderContents()),
+                                   extension)).replace(" ", "_").lstrip("\n").lower().replace("\n", "_")
+            file_contents = (urllib.urlopen(fq_url))
+            file_name = os.path.join(output_dir, file_name)
+            with open(file_name, "wb") as fp:
+                fp.write(file_contents.read())
+            os.chmod(file_name, 0644)
+            print "Saved %s..." % file_name
+
 
 if __name__ == "__main__":
   main() 
